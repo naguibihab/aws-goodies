8ffbcf81f9d64c32d790f286bd472b86cfbacb9d
EC2 Default User <ec2-user@ip-172-31-16-106.us-west-2.compute.internal>
2018-06-05 12:04:39 +0000

    Now getting the parameter from the url

--- Lambda/vanilla-golang/shoppingcart-example/get-item-from-inventory/main.go
+++ Lambda/vanilla-golang/shoppingcart-example/get-item-from-inventory/main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+  "log"
+  "encoding/json"
 	"github.com/aws/aws-lambda-go/lambda"
+  "github.com/aws/aws-lambda-go/events"
   
   "github.com/aws/aws-sdk-go/aws"
   "github.com/aws/aws-sdk-go/aws/session"
@@ -10,17 +13,15 @@ import (
   "github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 )
 
-type Request struct {
-	Name string `json:"name"`
-}
-
 type Item struct {
   Name string `json:"name"`
   Stock int `json:"stock"`
   Price float64 `json:"price"`
 }
 
-func Handler(request Request) (*Item, error) {
+func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+  
+  log.Printf("Processing Lambda request %s\n", request.PathParameters)
   
   sess, err := session.NewSession(&aws.Config{
     Region: aws.String("us-west-2")},
@@ -33,14 +34,14 @@ func Handler(request Request) (*Item, error) {
     TableName: aws.String("Inventory"),
     Key: map[string]*dynamodb.AttributeValue{
       "name": {
-          S: aws.String(request.Name),
+          S: aws.String(request.PathParameters["name"]),
       },
     },
   })
   
   if err != nil {
     fmt.Println(err.Error())
-    return nil, err
+//     return nil, err
   }
   
   item := Item{}
@@ -49,12 +50,12 @@ func Handler(request Request) (*Item, error) {
   
   if err != nil {
     panic(fmt.Sprintf("Failed to unmarshal Record, %v", err))
-    return nil, err
+//     return nil, err
   }
 
   if item.Name == "" {
       fmt.Println("Could not find item")
-      return nil, nil
+//       return nil, nil
   }
   
   fmt.Println("Found item:")
@@ -62,7 +63,26 @@ func Handler(request Request) (*Item, error) {
   fmt.Println("Stock: ", item.Stock)
   fmt.Println("Price:  ", item.Price)
   
-	return &item, nil
+  js, err := json.Marshal(item)
+//   if err != nil {
+//     return serverError(err)
+//   }
+  
+//   func serverError(err error) (events.APIGatewayProxyResponse, error) {
+//     errorLogger.Println(err.Error())
+
+//     return events.APIGatewayProxyResponse{
+//         StatusCode: http.StatusInternalServerError,
+//         Body:       http.StatusText(http.StatusInternalServerError),
+//     }, nil
+//   }
+  
+  return events.APIGatewayProxyResponse{
+    Headers:    map[string]string{"content-type": "application/json"},
+    Body:       string(js),
+    StatusCode: 200,
+  }, nil
+  
 }
 
 func main() {
